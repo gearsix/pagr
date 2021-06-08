7e48911d776cd383d9e2584b2b070bc6d06f3238
gearsix <gearsix@tuta.io>
2021-06-08 10:56:30 +0100

    added flags 'dir' and 'verbose'

--- pagr.go
+++ pagr.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+    "flag"
     "log"
 )
 
 const Name = "pagr"
 const Version = "0.0.0"
 
+var flagDir string
+var flagDirDesc = "directory of target project"
+var flagVerbose bool
+var flagVerboseDesc = "print verbose logs"
+
 func init() {
+    flag.StringVar(&flagDir, "d", ".", flagDirDesc)
+    flag.StringVar(&flagDir, "dir", ".", flagDirDesc)
+    flag.BoolVar(&flagVerbose, "v", false, flagVerboseDesc)
+    flag.BoolVar(&flagVerbose, "verbose", false, flagVerboseDesc)
+    flag.Parse()
 }
 
 func main() {
