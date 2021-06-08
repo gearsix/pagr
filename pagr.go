20ea2c7be0f160bd1558d012be247b6728f7fe1e
gearsix <gearsix@tuta.io>
2021-06-08 10:48:05 +0100

    init commit

---
+++ pagr.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "log"
+)
+
+const Name = "pagr"
+const Version = "0.0.0"
+
+func init() {
+}
+
+func main() {
+}
+
+func check(error) {
+    if err != nil {
+        log.Printf("ERROR! %s\n", err)
+    }
+    return err != nil
+}
